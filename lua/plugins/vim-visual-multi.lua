8d810b1447f11d93493649dc9ab0a6df5b8a62d8
nomura <s.nomura475565@gmail.com>
2026-02-03 07:44:51 +0900

    vim-visual-multi のキーバインド変更

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -8,10 +8,13 @@ return {
       -- 【上下】Alt-j/k でカーソルを追加
       ["Add Cursor Down"] = "<A-j>",
       ["Add Cursor Up"] = "<A-k>",
-      
+
       -- 【マウス】Alt + クリックでカーソル追加
       ["Mouse Cursor"] = "<A-LeftMouse>",
       ["Mouse Word"] = "<A-RightMouse>", -- (おまけ) 右クリックで単語選択
+
+      -- 【Find Under】カーソル下の単語を選択（デフォルト<C-n>から変更）
+      ["Find Under"] = "<Leader>n",
     }
 
     -- 以下の設定を入れておくと、日本語入力時などの不具合が減ります
